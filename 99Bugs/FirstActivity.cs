24eb2413cf223123d32741996124aa2281347aab
CallieAnn <dedorec@my.lanecc.edu>
2018-10-04 17:15:41 -0700

    Counts down bugs correctly until there are 0

---
+++ 99Bugs/FirstActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace _99Bugs
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class FirstActivity : AppCompatActivity
+    {
+        TextView bugLabel;
+        int bugTotal = 99;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+
+            var takeAwayButton = new Button(this);
+            takeAwayButton.SetText(Resource.String.takeAwayButtonText);
+
+            var takeAwayTwoButton = new Button(this);
+            takeAwayTwoButton.SetText(Resource.String.takeAwayTwoButtonText);
+
+            bugLabel = new TextView(this);
+            bugLabel.Text = "Squash the bugs!";
+            
+            layout.AddView(takeAwayButton);
+            layout.AddView(takeAwayTwoButton);
+            layout.AddView(bugLabel);
+            SetContentView(layout);
+
+            takeAwayButton.Click += (sender, e) =>
+            {
+                MakeClickIntent(1, 0);
+            };
+
+            takeAwayTwoButton.Click += (sender, e) =>
+            {
+                MakeClickIntent(2, 1);
+            };
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if(resultCode == Result.Ok)
+            {
+                bugLabel.Text = data.GetStringExtra("BugMessage");
+                bugTotal = data.GetIntExtra("BugCount", -1);
+            }
+        }
+
+        protected void MakeClickIntent(int numberClicked, int activityIndex)
+        {
+            var second = new Intent(this, typeof(SecondActivity));
+            second.PutExtra("FirstData", numberClicked);
+            if (bugTotal != -1)
+            {
+                second.PutExtra("BugTotal", bugTotal);
+            }
+
+            StartActivityForResult(second, activityIndex);
+        }
+
+
+       
+    }
+}
---
+++ 99Bugs/SecondActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace _99Bugs
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        TextView bugsLeftLabel;
+        int bugTotal;
+        protected override void OnCreate(Bundle bundle)
+        {
+            
+            base.OnCreate(bundle);
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+
+            var patchButton = new Button(this);
+            patchButton.SetText(Resource.String.patchButtonText);
+
+            int bugsGone = Intent.Extras.GetInt("FirstData");
+            
+            bugTotal = Intent.Extras.GetInt("BugTotal");
+            
+            
+            bugTotal = bugTotal - bugsGone;
+            
+
+            bugsLeftLabel = new TextView(this);
+            bugsLeftLabel.Text = bugTotal.ToString();
+
+            layout.AddView(patchButton);
+            layout.AddView(bugsLeftLabel);
+
+            patchButton.Click += (sender, e) =>
+            {
+                var myIntent = new Intent(this, typeof(FirstActivity));
+                myIntent.PutExtra("BugMessage",  bugTotal + " little bugs left");
+                myIntent.PutExtra("BugCount", bugTotal);
+                SetResult(Result.Ok, myIntent);
+                Finish();
+            };
+
+            SetContentView(layout);
+        }
+    }
+}
